e421cabf7e65960591b06056889046f571343e13
artit1981 <artit.g@live.com>
2016-02-26 08:16:01 +0700

    ProvinceControl

---
+++ UB/clsControl/ProvinceControl.vb
@@ -0,0 +1,200 @@
+﻿Option Explicit On
+Imports DevExpress.XtraEditors
+Imports System.Data.SqlClient
+Public Class ProvinceControl
+    Implements iControl
+    Private Const mCaption As String = "จังหวัด"
+    Public Sub Execute(ByVal pOrderType As Integer) Implements iControl.Execute
+        Try
+            mCtlForm = New frmControls
+            With mCtlForm
+                .Text = mCaption
+                .MdiParent = frmMain
+                .OptionBar.Visibility = DevExpress.XtraBars.BarItemVisibility.Never
+                Call LoadData()
+                Call LoadFilter()
+                .Show()
+            End With
+
+        Catch ex As Exception
+            ShowErrorMsg(False, ex.Message)
+        Finally
+        End Try
+    End Sub
+
+    Private WithEvents mCtlForm As frmControls
+    Private mFindName(1) As String
+    Private mCHECKSUM_AGG As Long
+
+    Private Sub LoadData()
+        Dim lcls As New ProvinceDAO
+        Dim dataTable As New DataTable()
+
+        Try
+
+
+            dataTable = lcls.GetDataTable()
+
+            If dataTable.Rows.Count > 0 Then
+                mCtlForm.GridControl.DataSource = dataTable
+                mCtlForm.GridView.ViewCaption = mCaption
+                Call GridStyle()
+            Else
+                mCtlForm.GridControl.DataSource = Nothing
+            End If
+
+        Catch e As Exception
+            Err.Raise(Err.Number, e.Source, "ProvinceControl.LoadData : " & e.Message)
+        Finally
+            lcls = Nothing
+            dataTable = Nothing
+        End Try
+    End Sub
+
+
+    Private Sub LoadFilter()
+        mFindName(0) = "ชื่อจังหวัด"
+        mFindName(1) = "ProvinceName"
+        Try
+            With mCtlForm
+                .cboFindIn.Properties.Items.Add(mFindName(0))
+                .cboFindIn.SelectedIndex = 0
+            End With
+
+        Catch e As Exception
+            Err.Raise(Err.Number, e.Source, "ProvinceControl.LoadFilter : " & e.Message)
+        Finally
+        End Try
+    End Sub
+
+    Private Sub GridStyle()
+        With mCtlForm.GridView
+
+            .Columns("ID").Width = 0
+            .Columns("ID").Caption = ""
+            .Columns("ID").Visible = False
+
+            .Columns("ProvinceCode").Visible = False
+
+            .Columns("ProvinceName").Caption = "ชื่อจังหวัด"
+            .Columns("ProvinceName").Width = 700
+
+            .Columns("ProvinceNameEng").Caption = "ชื่อจังหวัด (ENG.)"
+            .Columns("ProvinceNameEng").Width = 700
+
+        End With
+    End Sub
+
+
+    Private Sub mCtlForm_AddNew() Handles mCtlForm.AddNew
+        Try
+            Call ShowForm(DataMode.ModeNew, 0)
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.mCtlForm_AddNew : " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ShowForm(ByVal pMode As Integer, ByVal pID As Long)
+        Dim lFormEdit As frmProvince
+        Try
+            lFormEdit = New frmProvince
+            With lFormEdit
+                .Caption = mCaption
+                .MdiParent = frmMain
+                .ModeData = pMode
+                .IDs = pID
+                .Show()
+            End With
+
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.ShowForm : " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub mCtlForm_CHeckRefresh() Handles mCtlForm.CHeckRefresh
+        Dim lcls As New ProvinceDAO
+        Dim lCurrent As Long
+        Try
+            lCurrent = lcls.CHECKSUM_AGG
+            If mCHECKSUM_AGG <> lCurrent Then
+                LoadData()
+                mCHECKSUM_AGG = lCurrent
+            End If
+        Catch e As Exception
+            Err.Raise(Err.Number, e.Source, "ProvinceControl.mCtlForm_CHeckRefresh : " & e.Message)
+        Finally
+            lcls = Nothing
+        End Try
+    End Sub
+
+    Private Sub mCtlForm_Delete(ByRef pID As Long) Handles mCtlForm.Delete
+        Dim lcls As New ProvinceDAO
+
+        Try
+            If lcls.InitailData(pID) Then
+                If lcls.CheckIsToUse = False Then  ''ถูกใช้งานอยู่ ???
+                    lcls.ID = pID
+                    lcls.ModeData = DataMode.ModeDelete
+                    If lcls.SaveData() Then
+                        Call LoadData()
+                    End If
+                Else
+                    pID = 0
+                End If
+            End If
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.mCtlForm_Delete : " & ex.Message)
+        Finally
+            lcls = Nothing
+        End Try
+    End Sub
+
+    Private Sub mCtlForm_Edit(ByVal pID As Long) Handles mCtlForm.Edit
+        Try
+            Call ShowForm(DataMode.ModeEdit, pID)
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.mCtlForm_Edit : " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub mCtlForm_InitDetail(ByVal pID As Long) Handles mCtlForm.InitDetail
+        Dim lclsDepartmentDAO As New ProvinceDAO
+
+        Try
+            If lclsDepartmentDAO.InitailData(pID) Then
+
+                mCtlForm.UcInfo1.Init(lclsDepartmentDAO.GetInfoHtml, Nothing)
+            End If
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.mCtlForm_InitDetail : " & ex.Message)
+        Finally
+            lclsDepartmentDAO = Nothing
+        End Try
+    End Sub
+
+    Private Sub mCtlForm_RefreshFind() Handles mCtlForm.RefreshFind
+        Try
+            Dim findText As String = mCtlForm.GridView.FindFilterText
+            Dim lstrCon As String
+            lstrCon = ""
+            Select Case Trim(mCtlForm.cboFindIn.SelectedItem)
+              
+                Case mFindName(0)
+                    lstrCon = mFindName(1)
+            End Select
+            mCtlForm.GridView.OptionsFind.FindFilterColumns = lstrCon
+            mCtlForm.GridView.ApplyFindFilter(String.Empty)
+            mCtlForm.GridView.ApplyFindFilter(findText)
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.mCtlForm_ReLoad : " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub mCtlForm_ReLoad() Handles mCtlForm.ReLoad
+        Try
+            Call LoadData()
+        Catch ex As Exception
+            Err.Raise(Err.Number, ex.Source, "ProvinceControl.mCtlForm_ReLoad : " & ex.Message)
+        End Try
+    End Sub
+End Class
